da7f28e99617627c093f17de91d3e026883adb8f
Jan Möbius <moebius@cs.rwth-aachen.de>
2023-08-23 10:31:56 +0200

    Lowerd max allowed warnings for cppcheck

--- CI/ci-cppcheck.sh
+++ CI/ci-cppcheck.sh
@@ -32,7 +32,7 @@ echo "CPPCHECK Summary"
 echo "=============================================================================="
 echo -e "${NC}"
 
-MAX_COUNT=471
+MAX_COUNT=300
 
 if [ $COUNT -gt $MAX_COUNT ]; then
   echo -e ${WARNING}
