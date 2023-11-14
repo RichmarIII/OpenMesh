03ac240d32a48da2b9b7d1377e297b7444abb5e9
Jan Möbius <moebius@cs.rwth-aachen.de>
2023-11-14 11:00:39 +0100

    Fixed naming

--- CI/Windows.bat
+++ CI/Windows.bat
@@ -180,7 +180,7 @@ del *.exe
 
 IF %errorlevel% NEQ 0 exit /b %errorlevel%
 
-move OpenMesh-*.exe "OpenMesh-9.0-Git-Master-%CI_BUILD_REF%-%BUILD_PLATFORM%-%STRING_ARCH%%STRING_DLL%%STRING_APPS%.exe"
+move OpenMesh-*.exe "OpenMesh-10.0-Git-Master-%BUILD_PLATFORM%-%STRING_ARCH%%STRING_DLL%%STRING_APPS%.exe"
 
 
 
